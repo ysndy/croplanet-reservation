e49723d33c34652b7bc1642f428c120d222b7a86
ysndy <ysndy@naver.com>
2023-09-24 13:36:56 +0900

    fix: #2 Action / index -> id

--- src/main/java/croplanet/admin/action/service/ActionService.java
+++ src/main/java/croplanet/admin/action/service/ActionService.java
@@ -1,17 +1,14 @@
 package croplanet.admin.action.service;
 
 import croplanet.admin.action.domain.Action;
-import croplanet.admin.reservation.domain.Reservation;
 import croplanet.admin.reservation.repository.ReservationRepository;
 import croplanet.admin.action.repository.ActionRepository;
 import croplanet.admin.user.domain.User;
-import croplanet.admin.user.dto.KakaoDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
@@ -23,12 +20,12 @@ public class ActionService {
 
     public Long save(User user, String action, String query){
         Action userMethod = new Action(user, action, query);
-        return actionRepository.save(userMethod).getIndex();
+        return actionRepository.save(userMethod).getId();
     }
 
     public Long save(Action action){
 
-        return actionRepository.save(action).getIndex();
+        return actionRepository.save(action).getId();
     }
 
     public List<Action> getActions(){
