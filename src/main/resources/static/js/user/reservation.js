d58f11903a31be6ae5c7dcd3fda669abb3f3558b
ysndy <ysndy@naver.com>
2023-10-10 21:23:33 +0900

    fix: #2 사전예약 페이지 진입 시 채널 추가 창 안뜨도록 수정

--- src/main/resources/static/js/user/reservation.js
+++ src/main/resources/static/js/user/reservation.js
@@ -1,9 +1,6 @@
 <!-- 카카오 채널 추가 -->
 
 Kakao.init('ffb1ac4b290c42a760a2af6a06801cba'); // 사용하려는 앱의 JavaScript 키 입력
-Kakao.Channel.addChannel({
-    channelPublicId: '_gixiMG',
-});
 
 function addChannel(){
     //채널 추가
